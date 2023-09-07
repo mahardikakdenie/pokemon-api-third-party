a894f5bd77cd86eb6e4654d9b42f7db70fdc3abe
MahardiikaKD <dikamahar884@gmail.com>
2023-09-07 22:56:44 +0700

    mapping to show abilities

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -24,74 +24,21 @@ class PokemonController extends Controller
                 'limit' => $request->input('limit', 20),
                 'offset' => $request->input('offset', 0),
             ]);
-            return Json::response($res);
+            $data = $res['results'];
+            $result = array_map(function ($curr) {
+                $pokemon = Helper::get("{$this->endpoint}/" . $curr['name']);
+                $detail =
+                    $curr['abilities'] = $pokemon['abilities'];
+
+                return $curr;
+            }, $data);
+            return Json::response($result);
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             return Json::exception('Error Exceptions ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
         } catch (\Illuminate\Database\QueryException $e) {
             return Json::exception('Error Query ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
         } catch (\ErrorException $e) {
             return Json::exception('Error Exception ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
-        }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function addFavorite(Request $request)
-    {
-        try {
-            DB::beginTransaction();
-            $pokemonId = $request->pokemon_id;
-            $pokemonResponse = Helper::get("{$this->endpoint}/$pokemonId");
-            $pokemon = $pokemonResponse['forms'][0];
-            $abilities = $pokemonResponse['abilities'];
-
-            // dd($abilities);
-
-
-            $pokemonFavorite = new Favorite();
-            $pokemonFavorite->name = $pokemon['name'];
-            $pokemonFavorite->url = $pokemon['url'];
-            $pokemonFavorite->pokemon_id = $pokemonId;
-            $pokemonFavorite->save();
-
-            foreach ($abilities as $key => $value) {
-                $url = $value['ability']['url'];
-                $name = $value['ability']['name'];
-                $ability = new Ability();
-                $ability->name = $name;
-                $ability->url = $url;
-                $ability->favorite_id = $pokemonFavorite->id;
-                DB::commit();
-                $ability->save();
-            }
-
-
-            DB::commit();
-            return Json::response($pokemonFavorite);
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            DB::rollBack();
-            return Json::exception('Error Exceptions ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
-        } catch (\Illuminate\Database\QueryException $e) {
-            DB::rollBack();
-            return Json::exception('Error Query ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
-        } catch (\ErrorException $e) {
-            DB::rollBack();
-            return Json::exception('Error Exception ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
-        }
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function listFavorites(Request $request)
-    {
-        try {
-            $favorites = Favorite::entities($request->entities)->get();
-
-            return Json::response($favorites);
-        } catch (\Throwable $th) {
-            //throw $th;
         }
     }
 
